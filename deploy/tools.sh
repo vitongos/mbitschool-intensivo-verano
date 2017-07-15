2866d1d2af740646dd32c04e3600d1ac9cee66a5
Victor <victorsvv@yahoo.com>
2017-07-15 11:38:31 +0200

    Remove PackageKit.

--- deploy/tools.sh
+++ deploy/tools.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+systemctl stop packagekit
+yum remove -y PackageKit
 cd /tmp
 wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-10.noarch.rpm
 rpm -ihv epel-release-7-10.noarch.rpm
