335673c623d41ee1f09cd00f39627f3f0bf1d8c0
Victor Santiago <vitongos@users.noreply.github.com>
2018-07-13 14:37:02 +0200

    Update neo4j.sh

--- deploy/neo4j.sh
+++ deploy/neo4j.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
 cd /tmp
-wget -O neo4j.tar.gz http://neo4j.com/artifact.php?name=neo4j-community-3.2.2-unix.tar.gz
-tar zxf neo4j.tar.gz
-sudo mv neo4j-community-3.2.2/ /opt/neo4j/
-sudo ln -s /opt/neo4j/bin/neo4j /usr/bin/neo4j
-sed -i -r 's/#dbms\.security\.auth_enabled=false/dbms\.security\.auth_enabled=false/' /opt/neo4j/conf/neo4j.conf
-sed -i -r 's/#dbms\.allow_format_migration=true/dbms\.allow_format_migration=true/' /opt/neo4j/conf/neo4j.conf
-sed -i -r 's/#dbms\.shell\.enabled=true/dbms\.shell\.enabled=true/' /opt/neo4j/conf/neo4j.conf
+wget http://debian.neo4j.org/neotechnology.gpg.key
+rpm --import neotechnology.gpg.key
 wget http://example-data.neo4j.org/3.0-datasets/cineasts.tgz
 tar xzf cineasts.tgz
-mv graph.db /opt/neo4j/data/databases/
-chown centos:centos /opt/neo4j/data -R
-/opt/neo4j/bin/neo4j start
+
+cat <<EOF>  /etc/yum.repos.d/neo4j.repo
+[neo4j]
+name=Neo4j Yum Repo
+baseurl=http://yum.neo4j.org/stable
+enabled=1
+gpgcheck=1
+EOF
+
+yum install -y neo4j
+
+sed -i -r 's/#dbms\.security\.auth_enabled=false/dbms\.security\.auth_enabled=false/' /etc/neo4j/neo4j.conf
+sed -i -r 's/#dbms\.allow_upgrade=true/dbms\.allow_upgrade=true/' /etc/neo4j/neo4j.conf
+sed -i -r 's/#dbms\.shell\.enabled=true/dbms\.shell\.enabled=true/' /etc/neo4j/neo4j.conf
+sed -i -r 's/#dbms\.security\.allow_csv_import_from_file_urls=true/dbms\.security\.allow_csv_import_from_file_urls=true/' /etc/neo4j/neo4j.conf
+
+service neo4j restart
+
+sleep 20
+
+service neo4j stop
+
+mv /tmp/graph.db/* /var/lib/neo4j/data/databases/graph.db/ -f
+chown neo4j:neo4j /var/lib/neo4j/data/databases/graph.db/ -R
+
+service neo4j start
