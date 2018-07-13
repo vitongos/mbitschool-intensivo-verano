a6535d6c5574b6a9c1c9f2be04693d976c2ba704
Victor Santiago <vitongos@users.noreply.github.com>
2018-07-13 12:39:26 +0200

    Update tools.sh

--- deploy/tools.sh
+++ deploy/tools.sh
@@ -3,7 +3,7 @@
 systemctl stop packagekit
 yum remove -y PackageKit
 cd /tmp
-wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-10.noarch.rpm
-rpm -ihv epel-release-7-10.noarch.rpm
+wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-11.noarch.rpm
+rpm -ihv epel-release-7-11.noarch.rpm
 yum install -y htop
 usermod -a -G vboxsf centos
